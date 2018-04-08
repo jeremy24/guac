6b14e8d5d4131c86ea333d27dbea126e903cd0ec
Nicklas <nredheff@vols.utk.edu>
2018-04-08 19:22:18 -0400

    Fingerprint handler added

--- frontend/app/src/main/java/cs402/guac/StartScreen.java
+++ frontend/app/src/main/java/cs402/guac/StartScreen.java
@@ -1,13 +1,30 @@
 package cs402.guac;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartScreen extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openact();
+            }
+        });
     }
+    public void openact(){
+        Intent intent = new Intent(this,MainActivity.class);
+//        ((Activity) getBaseContext()).finish();
+        startActivity(intent);
+    }
+    
 }

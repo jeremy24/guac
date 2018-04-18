16f2a9f5797f98a82dc8713a44b93c8da49111dc
Jeremy <jpoff1@vols.utk.edu>
2018-04-18 10:15:48 -0400

    added decorators, added crypto stuff

--- backend/app/app.py
+++ backend/app/app.py
@@ -5,12 +5,20 @@ from flask import Response
 from flask import json
 
 from Crypto.Signature import pkcs1_15
+from Crypto.Signature import DSS
+
 from Crypto.Hash import SHA256
+from Crypto.Hash import BLAKE2b
+
+
 from Crypto.PublicKey import RSA
+from Crypto.PublicKey import ECC
 
 import pymysql
 import bcrypt
 import time
+import functools
+import sys
 
 # Classes
 
@@ -30,6 +38,81 @@ class Code:
 
     authorize_trans = Response("Transaction authorized", status=200)
     fail_trans = Response("Transaction not authorized", status=200)
+
+
+
+def doublewrap(fn):
+    """
+    A decorator decorator, allowing to use the decorator to be used without
+    parentheses if not arguments are provided. All arguments must be optional.
+    """
+
+    @functools.wraps(fn)
+    def decorator(*args, **kwargs):
+        if len(args) == 1 and len(kwargs) == 0 and callable(args[0]):
+            return fn(args[0])
+        else:
+            return lambda wrapee: fn(wrapee, *args, **kwargs)
+
+    return decorator
+
+
+@doublewrap
+def error_wrap(fn, do_exit=False):
+    """
+    A decorator for functions that catches errors thrown by a function.
+        It has an optional parameter for whether to exit the program if an error is thrown.
+    """
+    @functools.wraps(fn)
+    def _wrapper(*args, **kwargs):
+        try:
+            return fn(*args, **kwargs)
+        except Exception as ex:
+            print("ERROR {0}:  ".format(fn.__name__), ex)
+            if do_exit:
+                sys.exit(1)
+            return None
+    return _wrapper
+
+
+@error_wrap(do_exit=True)
+def check_keys(data, keys=list()):
+    for key in keys:
+        if key not in data.keys():
+            print("Missing key: {0}".format(str(key)))
+            return False
+        if len(data) != len(list(data.keys())):
+            print("Data is of invalid length")
+            return False
+    return True
+
+
+@error_wrap(do_exit=False)
+def verify_password(plain_password, password_hash):
+    return bcrypt.checkpw(plain_password.encode(), password_hash.encode())
+
+
+@error_wrap(do_exit=True)
+def verify_ecc_signature(message, public_key_string, signature):
+    if type(message) != str:
+        print("messaged passed to verify_ecc_signature must be a string")
+        return False
+
+    pub_key = ECC.import_key(public_key_string)
+    hashed = SHA256.new(message.encode("utf-8"))
+
+    verifier = DSS.new(pub_key, 'fips-186-3')
+    try:
+        verifier.verify(hashed, signature)
+        print("The message is authentic.")
+        return True
+    except ValueError:
+        print("The message is not authentic.")
+        return False
+
+
+
+
 
 
 class Server:
@@ -59,6 +142,7 @@ server_app = Server("home.piroax.com", 9003, 'root', 'password', APP)
 
 def query_db(query):
     try:
+        print("QUERY: ", query)
         with server_app.db_conn.cursor() as cursor:
             cursor.execute(query)
             a = cursor.fetchall()
@@ -66,6 +150,7 @@ def query_db(query):
             return a
     except Exception as ex:
         print("Query_db error: {0}".format(ex))
+        server_app.db_conn.rollback()
         if ex.args[0] == 1644:
             return 1644
         raise ex
@@ -74,7 +159,7 @@ def query_db(query):
 
 @APP.route('/')
 def hello_world():
-    return 'This is not the right way to access the volcard app...!'
+    return 'This is not the right way to access the VolCard app...!'
 
 
 
@@ -82,44 +167,30 @@ def hello_world():
 def add_user():
     try:
         data = request.get_json()
+        keys = ["username", "password", "public_key"]
+
+        if not check_keys(data=data, keys=keys):
+            return Code.bad_request
 
         print("/user/add:  {0}".format(data))
 
-        if "username" not in data.keys():
-            return Code.bad_request
-        if 'password' not in data.keys():
-            return Code.bad_request
-        if 'public_key' not in data.keys():
-            key = 'NULL'
-        else:
-            key = data['public_key']
-
         username = data["username"]
         password = data['password']
+        key = data["public_key"]
 
-        p = password
-        print("password:", p)
-
+        # print("password:", password)
         start = time.time()
-
-        
-
         salt = bcrypt.gensalt(rounds=WORK_FACTOR)
-        password = bcrypt.hashpw(str(password).encode(), salt)
+        hashed = bcrypt.hashpw(str(password).encode(), salt)
         end = time.time()
-        
+
         # print("pass: ", password)
-
         print("Time to hash with work factor: {0}  {1}".format(WORK_FACTOR, end-start))
-
-
-        salt=None
-
-        print(bcrypt.checkpw(p.encode(), password))
-
+        print("hashed correctly: ", bcrypt.checkpw(password.encode(), hashed))
         print("Trying to add the user")
+
         try:
-            query_str = "CALL add_user('{0}', '{1}', '{2}')".format(username, password.decode('utf-8'), key)
+            query_str = "CALL add_user('{0}', '{1}', '{2}')".format(username, hashed.decode('utf-8'), key)
             print("Query: {0}".format(query_str))
         except Exception as ex:
             print("Query Error: {0}".format(ex))
@@ -136,10 +207,10 @@ def add_user():
         return Code.server_error
 
 
+
 def _get_user(user=None):
     try:
         username = str(user)
-
         query_str = "CALL get_user('{0}')".format(username)
         res = query_db(query_str)
 
@@ -147,59 +218,48 @@ def _get_user(user=None):
         if len(res):
             resp = {'username': username, "public_key": res[0]["public_key"], "password": res[0]["password"]}
             return resp
-            
+
         return None
     except Exception as ex:
         print("_get_user", ex)
         return None
 
 
-def _verify_password(password, hash):
-    return bcrypt.checkpw(password.encode(), hash.encode())
 
-
-def validate(username, message, signature):
-    user = _get_user(username)
+@error_wrap(do_exit=True)
+def validate(_username, message, _signature):
+    user = _get_user(_username)
     key_str = user['public_key']
 
+    key = RSA.import_key(key_str)
+    print("key", key)
+    digest = SHA256.new(message)
+    print("digest", digest)
     try:
-        key = RSA.import_key(key_str)
-        print("key", key)
-        digest = SHA256.new(message)
-        print("digest", digest)
-        try:
-            pkcs1_15.new(key).verify(digest, signature)
-            print ("The signature is valid.")
-            return True
-        except (ValueError, TypeError):
-            print ("The signature is not valid.")
-            return False
-    except Exception as ex:
-        print("validate error: ", ex)
-        return None
+        pkcs1_15.new(key).verify(digest, _signature)
+        print("The signature is valid.")
+        return True
+    except (ValueError, TypeError):
+        print("The signature is not valid.")
+        return False
 
 
 @APP.route("/message/validate", methods=["POST"])
 def validate_message():
     try:
         data = request.get_json()
+        keys = ["username", "message", "signature"]
 
-        if "username" not in data.keys():
-            return Code.bad_request
-        if "message" not in data.keys():
-            return Code.bad_request
-        if "signature" not in data.keys():
-            return Code.bad_request
-        if len(list(data.keys())) != 3:
+        if not check_keys(data=data, keys=keys):
             return Code.bad_request
 
         username = data["username"]
         message = data["message"]
         signature = data["signature"]
 
-        # dont let them just throw random stuff at us
+        # don't let them just throw random stuff at us
         if _get_user(username) is None:
-            return code.not_authorized
+            return Code.not_authorized
 
         res = validate(username, message, signature)
         if res == True:
@@ -207,7 +267,6 @@ def validate_message():
         elif res is None: # returning None from this is an error state
             return Code.server_error
         return Code.fail_trans
-
 
     except Exception as ex:
         print("/user/get", ex)
@@ -217,61 +276,51 @@ def validate_message():
 @APP.route("/user/addkey", methods=["POST"])
 def add_user_key():
     try:
-        print("")
         data = request.get_json()
+        print("data", data)
+        keys = ["username", "public_key", "password"]
 
-        # print("data:", data)
-        # print("keys", data.keys())
-        if "username" not in data.keys():
-            print("missing username")
+        if not check_keys(data, keys):
             return Code.bad_request
-        if "public_key" not in data.keys():
-            print("missing key")
-            return Code.bad_request
-        if "password" not in data.keys():
-            print("missing password")
-            return Code.bad_request
-        if len(list(data.keys())) != 3:
-            print("wrong length")
-            return Code.bad_request
+
+        print("checked")
 
         username = data['username']
         plain_pass = data["password"]
         key = data["public_key"]
 
         user = _get_user(user=username)
-        print("user: ", user)
-
-        if not _verify_password(plain_pass, user["password"]):
-            return Code.not_authorized
 
         if user is None:
             return Code.user_not_found
 
-        query = "CALL add_user_key('{0}', '{1}')".format(data['username'], data['public_key'])
+        hashed_password = user["password"]
+
+        if not verify_password(plain_pass, hashed_password):
+            return Code.not_authorized
+
+        query = "CALL add_user_key('{0}', '{1}')".format(username, key)
+        print(query)
         res = query_db(query)
 
         if len(res) == 1:
             return Code.success
         return Code.server_error
     except Exception as ex:
-        print("/user/addkey", ex)
+        print("Error /user/addkey:  ", ex)
         return Code.server_error
-
 
 
 @APP.route("/user/get", methods=["POST"])
 def get_user():
     try:
         data = request.get_json()
+        keys = ["username"]
 
-        if "username" not in data.keys():
-            return Code.bad_request
-        if len(list(data.keys())) != 1:
+        if not check_keys(data=data, keys=keys):
             return Code.bad_request
 
         username = data["username"]
-
         query_str = "CALL get_user('{0}')".format(username)
         res = query_db(query_str)
 
@@ -289,4 +338,39 @@ def get_user():
 
 
 if __name__ == '__main__':
-    server_app.start()
+    # server_app.start()
+    key = ECC.generate(curve="P-256")
+    print(key.public_key().export_key(format="OpenSSH"))
+    pub = key.public_key()
+
+    raw_msg = "user=bob,transaction=145368,endpoint=00015,type=purchase,amount=15.35,time={}".format(str(time.time()))
+    msg = raw_msg.encode("utf-8")
+
+    # h = SHA256.new(b"test").digest()
+    h = SHA256.new(msg)
+    print(h)
+    # h.update(msg)
+    # print(h.digest())
+    # h = h.digest()
+
+    print(pub)
+
+    signer = DSS.new(key, 'fips-186-3')
+    signature = signer.sign(h)
+    print("Sig: ", signature)
+
+    pub_key_str = pub.export_key(format="OpenSSH")
+
+    print(pub_key_str)
+
+    time.sleep(1)
+    verify_ecc_signature(raw_msg, pub_key_str, signature)
+
+    # verifier = DSS.new(pub, 'fips-186-3')
+    # try:
+    #     verifier.verify(h, signature)
+    #     print ("The message is authentic.")
+    # except ValueError:
+    #     print ("The message is not authentic.")
+
+
